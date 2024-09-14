4d3711882e6f4d8e30040e9ac5c50a55502ff211
AmeliaYasmin <ameliayasminking@gmail.com>
2024-09-14 14:46:13 +0100

    Initial commit

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     
-const images = document.querySelectorAll("img");
-
-/* change every src property */
-
-for(const image of images){
-    fetch("https://dog.ceo/api/breeds/image/random")
-    .then(response => response.json())
-    .then(data => {
-        image.src = data.message
-        image.width = 100;
-        image.height = 100;
-    })
+    const images = document.querySelectorAll("img");
     
-
-}
-
+    /* change every src property */
+    
+    for(const image of images){
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => {
+            image.src = data.message
+            image.width = 100;
+            image.height = 100;
+        })
+        
+    
+    }
+    
 })
